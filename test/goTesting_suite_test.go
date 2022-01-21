49030af3701756f140aa8d3bb4bd3cfc456ff27f
DiptoChakrabarty <diptochuck123@gmail.com>
2022-01-22 00:17:50 +0530

    before and after

--- test/goTesting_suite_test.go
+++ test/goTesting_suite_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"goTest/human"
+	"log"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -15,8 +16,24 @@ func TestGoTesting(t *testing.T) {
 
 var _ = Describe("Person is a Child", func() {
 	Context("When Person is a child", func() {
+		AfterEach(func() {
+			log.Println("This is child")
+		})
 		It("returns true", func() {
-			person := human.Person{Age: 10}
+			person := human.Person{Age: 9}
+
+			response := person.IsChild()
+
+			Expect(response).To(BeTrue())
+		})
+	})
+
+	Context("When Person is not child", func() {
+		BeforeEach(func() {
+			log.Println("not child instance")
+		})
+		It("returns false", func() {
+			person := human.Person{Age: 35}
 
 			response := person.IsChild()
 
