e3bdfc565229c299b0b300e484b8458a6f2279d5
DiptoChakrabarty <diptochuck123@gmail.com>
2022-01-26 18:22:17 +0530

    interface errors and some methods

--- test/human_test.go
+++ test/human_test.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
+	//	. "github.com/onsi/ginkgo/v2"
+
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
 )
+
+type HumanBeing interface {
+	Name() string
+}
 
 func TestHumanStruct(t *testing.T) {
 	tests := []struct {
@@ -25,7 +30,7 @@ func TestHumanStruct(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			g := NewWithT(t)
 			p1 := Person{name: "Pinku"}
-			g.Expect(p1).To(ContainElement(MatchFirstName(tt.name)))
+			g.Expect(p1.Name()).To(Equal(MatchFirstName(tt.name)))
 		})
 	}
 }
@@ -34,13 +39,28 @@ type Person struct {
 	name string
 }
 
-func MatchFirstName(s string) types.GomegaMatcher {
-	return &Person{s}
+func (p *Person) Name() string {
+	return p.name
 }
 
-func (p *Person) Match(actual string) (bool, error) {
-	if p.name != actual {
+func MatchFirstName(s string) types.GomegaMatcher {
+	fmt.Println(s)
+	return &Person{name: s}
+}
+
+func (p *Person) Match(actual interface{}) (bool, error) {
+	fmt.Println("This is actuval", actual)
+	fmt.Println("This is person", p)
+	if p.name != actual.(string) {
 		return false, fmt.Errorf("Wrong Person")
 	}
 	return true, nil
+}
+
+func (p *Person) FailureMessage(actual interface{}) string {
+	return fmt.Sprintf("Expected name to be %v but received %v", actual, p.name)
+}
+
+func (p *Person) NegatedFailureMessage(actual interface{}) string {
+	return fmt.Sprintf("Expected name to be %v but received %v", actual, p.name)
 }
