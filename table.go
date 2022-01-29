1232480f251b763eb1801155de82fef4be4c603a
DiptoChakrabarty <diptochuck123@gmail.com>
2022-01-29 10:36:49 +0530

    attempt to compare tables

--- table.go
+++ table.go
@@ -1,53 +1,61 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
+	"bytes"
 	"fmt"
-	"os"
 
 	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
 	fmt.Println("This is New Table")
-	stdout := bufio.NewWriter(os.Stdout)
+	//stdout := bufio.NewWriter(os.Stdout)
+	var output bytes.Buffer
 
 	//tableString := &strings.Builder{}
 	data := [][]string{
 		[]string{"1/1/2014", "Domain name", "2233", "$10.98"},
-		[]string{"1/1/2014", "January Hosting", "2233", "$54.95"},
-		[]string{"1/4/2014", "February Hosting", "2233", "$51.00"},
-		[]string{"1/4/2014", "February Extra Bandwidth", "2233", "$30.00"},
 	}
 
-	table := tablewriter.NewWriter(stdout)
+	table := tablewriter.NewWriter(&output)
 	table.SetHeader([]string{"Date", "Description", "CV2", "Amount"})
 	table.SetFooter([]string{"", "", "Total", "$146.93"}) // Add Footer
 	table.SetBorder(false)                                // Set Border to false
 	table.AppendBulk(data)
 
 	table.Render()
-	fmt.Println(stdout.Size())
+	tbl := output.String()
+	fmt.Println(tbl)
+	fmt.Println(len(tbl))
+	//for i, _ := range tbl {
+	//	fmt.Println(string(tbl[i]))
+	//}
+	//fmt.Println(stdout.Size())
+	// https://stackoverflow.com/questions/26804642/how-to-test-a-functions-output-stdout-stderr-in-unit-tests
+	// https://stackoverflow.com/questions/10473800/in-go-how-do-i-capture-stdout-of-a-function-into-a-string
+
 	//fmt.Println(tableString.String())
 	//fmt.Println(len(tableString.String()))
 
-	/*
-		random := []string{
-			"Object/root",
-			"├─Object/child1",
-			"│ │           ├─C1.1", // first condition child gets pipes, children pipe and ├─
-			"│ │           └─C1.2", // last condition child gets pipes, children pipe and └─
-			"│ └─Object/child1.1",
-			"└─Object/child2",
-			"  │           ├─C2.1", // first condition child gets spaces, children pipe and ├─
-			"  │           └─C2.2", // last condition child gets spaces, children pipe and └─
-			"  └─Object/child2.1",
-		}
+	random := []string{
+		"DATE   | DESCRIPTION |  CV2  | AMOUNT",
+		"-----------+-------------+-------+----------",
+		"  1/1/2014 | Domain name |  2233 | $10.98",
+		"-----------+-------------+-------+----------",
+		"						   TOTAL | $146.93",
+		"						 --------+----------",
+	}
 
-		for i := range random {
-			for j := range random[i] {
-				fmt.Println(string(random[i][j]))
+	for i := range random {
+		for j := range random[i] {
+			for i := range tbl {
+				if tbl[i] != random[i][j] {
+					fmt.Println("Not Matching Correctly")
+					break
+				}
 			}
-			//fmt.Println("\n")
-		}*/
+		}
+		//fmt.Println("\n")
+	}
 }
