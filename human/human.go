112c50397be042eb52e5c1bf7cde07aa1347164a
DiptoChakrabarty <diptochuck123@gmail.com>
2022-02-12 21:11:27 +0530

    move person struct

--- human/human.go
+++ human/human.go
@@ -1,11 +1,6 @@
 package human
 
+// Person Struct to Compare
 type Person struct {
-	Age       int
-	FirstName string
-	LastName  string
-}
-
-func (p *Person) IsChild() bool {
-	return p.Age < 10
+	age []int
 }
--- human_test.go
+++ human_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/DiptoChakrabarty/human"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
 )
@@ -12,19 +13,19 @@ func TestHumanStruct(t *testing.T) {
 	tests := []struct {
 		name        string
 		targetAge   []int
-		humanTarget Person
+		humanTarget human.Person
 		want        bool
 	}{
 		{
 			name:        "Compare Age Correct",
 			targetAge:   []int{10, 20},
-			humanTarget: Person{age: []int{10, 20}},
+			humanTarget: human.Person{age: []int{10, 20}},
 			want:        true,
 		},
 		{
 			name:        "Compare Age Incorrect",
 			targetAge:   []int{11, 21},
-			humanTarget: Person{age: []int{10, 20}},
+			humanTarget: human.Person{age: []int{10, 20}},
 			want:        false,
 		},
 	}
@@ -41,21 +42,16 @@ func TestHumanStruct(t *testing.T) {
 	}
 }
 
-// Person Struct to Compare
-type Person struct {
-	age []int
-}
-
 // MatchAge is the Custom Gomega Testing Function
 func MatchAge(a []int) types.GomegaMatcher {
-	return &Person{age: a}
+	return &human.Person{age: a}
 }
 
 // Match expects the actual item which is compared to the
 // target returned from the custom Gomega function
-func (p *Person) Match(actual interface{}) (bool, error) {
+func (p *human.Person) Match(actual interface{}) (bool, error) {
 	switch actual := actual.(type) {
-	case Person:
+	case human.Person:
 		for i, j := range actual.age {
 			if j != p.age[i] {
 				return false, fmt.Errorf("Wrong Person")
@@ -65,10 +61,10 @@ func (p *Person) Match(actual interface{}) (bool, error) {
 	return true, nil
 }
 
-func (p *Person) FailureMessage(actual interface{}) string {
+func (p *human.Person) FailureMessage(actual interface{}) string {
 	return fmt.Sprintf("Expected age to be %v but received %v", actual, p.age)
 }
 
-func (p *Person) NegatedFailureMessage(actual interface{}) string {
+func (p *human.Person) NegatedFailureMessage(actual interface{}) string {
 	return fmt.Sprintf("Expected age to be %v but received %v", actual, p.age)
 }
